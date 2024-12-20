36cee764089bad541cb4bb26b388ef4c00850595
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-12-20 19:37:00 +0300

    Arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+// Function expression for divide
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function for square
+  const square = x => x * x;
+  
+  // Arrow function for add
+  const add = (a, b) => a + b;
+  
+
